66af36b0312d51872d12c62d01bf6f0b5f3a3f89
PERONAL <PERONAL@192.168.1.78>
2022-04-06 11:23:56 -0500

    operaciones basicas

---
+++ src/Operaciones/Avanzado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+
+/**
+ *
+ * @author PERONAL
+ */
+public class Avanzado {
+    
+    public double potencia(int base, int elevacion){
+        return Math.pow(base, elevacion);
+    }
+    
+}
---
+++ src/Operaciones/Basicas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+
+/**
+ *
+ * @author PERONAL
+ */
+public class Basicas {
+    
+    public int Sumar(int num1, int num2){
+        return (num1+num2);
+    }
+    
+    public int Resta(int a, int b){
+        return(a-b);
+    }
+    
+}
